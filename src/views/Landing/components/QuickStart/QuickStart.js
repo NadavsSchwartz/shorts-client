23006985768ac88d9f56d2d6e4acd7721871b7a4
nadavschwartz <nadavschwartz58@gmail.com>
2022-03-22 17:36:49 -0700

    Update QuickStart.js

--- src/views/Landing/components/QuickStart/QuickStart.js
+++ src/views/Landing/components/QuickStart/QuickStart.js
@@ -5,7 +5,7 @@ import { vs2015 } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-
+import { API } from '../../../../consts/index';
 const QuickStart = () => {
   const theme = useTheme();
   return (
@@ -46,12 +46,12 @@ const QuickStart = () => {
         bgcolor={'#21325b !important'}
       >
         {`
-> $ curl -XPOST -H "Content-type: application/json" -d '{"longUrl":"https://www.google.com"}' 'https://shorten.domains/url'
+> $ curl -XPOST -H "Content-type: application/json" -d '{"longUrl":"https://www.google.com"}' '${API.BASE}/url'
 
-// {"urlId":"yDDqG2s","longUrl":"https://www.google.com","shortUrl":"https://shorten.domains/Qri0_bg","siteIcon":"https://www.google.com/favicon.ico"}
+// {"urlId":"yDDqG2s","longUrl":"https://www.google.com","shortUrl":${API.BASE}/Qri0_bg,"siteIcon":"https://www.google.com/favicon.ico"}
 
 
-> $ curl -XGET -H "Content-type: application/json" 'https://shorten.domains/url/yDDqG2s'
+> $ curl -XGET -H "Content-type: application/json" '${API.BASE}/url/yDDqG2s'
 
 // {"_id":"6233e5a5b3a12ac378c90c93","totalClicks":0,"location":[],"clicks":[],"createdAt":"2022-03-18T01:51:33.932Z","updatedAt":"2022-03-18T01:51:34.075Z"}
         `}
